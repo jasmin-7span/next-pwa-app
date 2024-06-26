1bd1fd99bfc3e332600d8a577ee1dd3a05159cda
jasmin-7span <jasmin@7span.com>
2024-06-26 14:44:22 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public'
+})
+
+const nextConfig = {
+  reactStrictMode: false,
+};
+
+module.exports = withPWA(nextConfig);
